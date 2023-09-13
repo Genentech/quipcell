ea3ef880cdb335bb6d535fedc36ddb034c604c49
Jack Kamm <jackkamm@gmail.com>
2023-09-13 09:15:25 -0700

    Add a simple unit test

---
+++ test/generate_test_example.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+import scDensQP as scqp
+
+mu1 = np.array([1,0])
+mu2 = np.array([0,1])
+sigma = .1
+
+mu = np.array([
+    [.75, .25],
+    [.5, .5],
+    [.1, .9]
+])
+
+assert np.allclose(mu.sum(axis=1), 1)
+
+rng = np.random.default_rng(12345)
+
+n = 10
+
+x1 = mu1 + sigma * rng.normal(size=(n, 2))
+x2 = mu2 + sigma * rng.normal(size=(n, 2))
+
+x = np.vstack([x1, x2])
+
+w = scqp.estimate_weights_multisample(x, mu)
+
+assert np.allclose(w.sum(axis=0), 1)
+
+assert np.all(n*w[:n, 0] < .85) and np.all(n*w[:n, 0] > .65)
+assert np.all(n*w[n:, 0] < .35) and np.all(n*w[n:, 0] > .15)
+
+assert np.all(n*w[:,1] > .4) and np.all(n*w[:,1] < .6)
+
+assert np.all(n*w[:n, 2] < .25) and np.all(n*w[n:, 2] > .8)
+
+np.savetxt("test_example_mu.txt", mu)
+np.savetxt("test_example_x.txt", x)
+np.savetxt("test_example_w.txt", w)
---
+++ test/test_example.py
@@ -0,0 +1,29 @@
+
+import pytest
+
+import os
+
+import numpy as np
+import scDensQP as scqp
+
+dirname = os.path.dirname(os.path.realpath(__file__))
+
+def test_example():
+    w = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_w.txt'
+    ))
+
+    x = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_x.txt'
+    ))
+
+    mu = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_mu.txt'
+    ))
+
+    w2 = scqp.estimate_weights_multisample(x, mu)
+
+    assert np.allclose(w, w2)
