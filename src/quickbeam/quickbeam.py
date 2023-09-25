1e4835f84e3fadb199a5b423e7df14c48b8c9a98
Jack Kamm <jackkamm@gmail.com>
2023-09-25 13:16:25 -0700

    Add docstrings

--- src/quickbeam/quickbeam.py
+++ src/quickbeam/quickbeam.py
@@ -14,9 +14,25 @@ from sklearn.preprocessing import OneHotEncoder
 
 logger = logging.getLogger(__name__)
 
-def estimate_weights_multisample(X, mu_multisample,
-                                 renormalize=True,
-                                 **kwargs):
+# TODO: parametrize the cvxpy.Problem by mu to reduce compilation times?
+# https://www.cvxpy.org/tutorial/advanced/index.html#disciplined-parametrized-programming
+def estimate_weights_multisample(
+        X, mu_multisample,
+        renormalize=True,
+        **kwargs
+):
+    """Estimate density weights for multiple samples on a single-cell reference.
+
+    Returns a numpy array with rows=cells, columns=samples.
+
+    ``**kwargs`` are passed onto :meth:`quickbeam.estimate_weights`.
+
+    :param `numpy.ndarray` X: Reference embedding. Rows=cells, columns=features.
+    :param `numpy.ndarray` mu_multisample: Sample moments. Either bulk gene counts (for bulk deconvolution) or sample centroids of single cells (for differential abundance). Rows=samples, columns=features.
+    :param bool renormalize: Correct for any solver inaccuracies by setting small negative numbers to 0, and renormalizing sums to 1.
+
+    :rtype: :class:`numpy.ndarray`
+    """
     start = datetime.now()
 
     w_hat_multisample = []
@@ -45,7 +61,18 @@ def estimate_weights_multisample(X, mu_multisample,
 
     return ret
 
+# TODO: Add accelerated projected gradient descent solver? E.g. see:
+# https://stackoverflow.com/questions/65526377/cvxpy-returns-infeasible-inaccurate-on-quadratic-programming-optimization-proble
 def estimate_weights(X, mu, quad_form=True, solve_kwargs=None):
+    """Estimate density weights for a single sample on a single-cell reference.
+
+    :param `numpy.ndarray` X: Reference embedding. Rows=cells, columns=features.
+    :param `numpy.ndarray` mu: Sample moments. Either bulk gene counts (for bulk deconvolution) or sample centroids of single cells (for differential abundance). Should be a 1-dimensional array.
+    :param bool quad_form: Whether to write the objective as a quadratic form, which allows using QP solvers, or as a norm, which only works with conic solvers.
+    :param dict solve_kwargs: Additional kwargs to pass to `cvxpy.Problem.solve`.
+
+    :rtype: :class:`numpy.ndarray`
+    """
     # Reference for solver options:
     # https://www.cvxpy.org/tutorial/advanced/index.html#setting-solver-options
     if not solve_kwargs:
@@ -92,19 +119,33 @@ def estimate_weights(X, mu, quad_form=True, solve_kwargs=None):
 
     return prob
 
-# TODO: parametrize the cvxpy.Problem by mu to reduce compilation times?
-# https://www.cvxpy.org/tutorial/advanced/index.html#disciplined-parametrized-programming
-
-# TODO: Add accelerated projected gradient descent solver? E.g. see:
-# https://stackoverflow.com/questions/65526377/cvxpy-returns-infeasible-inaccurate-on-quadratic-programming-optimization-proble
-
 def renormalize_weights(weights, size_factors):
+    """Renormalize weights to correct for cell-specific size factors.
+
+    This can be used to convert read-level probability weights to
+    cell-level weights.
+
+    :param `numpy.ndarray` weights: Weights as returned by `quickbeam.estimate_weights_multisample`. Rows=cells, columns=samples.
+    :param `numpy.ndarray` size_factors: Cell-level size factors. Should be 1 dimensional, with length equal to the number of rows in `weights`.
+
+    :rtype: :class:`numpy.ndarray`
+    """
     weights = np.einsum("ij,i->ij", weights, 1/size_factors)
     weights = np.einsum("ij,j->ij", weights, 1/weights.sum(axis=0))
     assert np.allclose(weights.sum(axis=0), 1)
     return weights
 
 def estimate_size_factors(X, n_reads, sample, **kwargs):
+    """Estimate cell-level size factors with Poisson regression to control for technical variation between samples.
+
+    Returns a vector of the size factor for each cell.
+
+    :param `numpy.ndarray` X: Single cell embedding. Rows=cells, columns=features.
+    :param `numpy.ndarray` n_reads: 1d vector of the number of reads per cell.
+    :param `numpy.ndarray` sample: 1d string vector for the sample that each cell came from.
+
+    :rtype: :class:`numpy.ndarray`
+    """
     enc = OneHotEncoder()
 
     modmat = enc.fit_transform(sample.reshape(-1, 1))
