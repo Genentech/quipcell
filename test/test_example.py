0995683412c872d8b920da8c158c27782b102d9b
Jack Kamm <jackkamm@gmail.com>
2024-01-02 12:33:46 -0800

    Add general alpha divergence

--- test/test_example.py
+++ test/test_example.py
@@ -4,6 +4,7 @@ import pytest
 import os
 
 import numpy as np
+import cvxpy as cp
 import quipcell as qpc
 
 dirname = os.path.dirname(os.path.realpath(__file__))
@@ -24,6 +25,101 @@ def test_example():
         'test_example_mu.txt'
     ))
 
-    w2 = qpc.estimate_weights_multisample(x, mu)
+    w2 = qpc.estimate_weights_multisample(
+        x, mu,
+        solve_kwargs={'solver': cp.OSQP}
+    )
+
+    assert np.allclose(w, w2)
+
+def test_example_relaxed():
+    w = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_w_relaxed.txt'
+    ))
+
+    x = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_x.txt'
+    ))
+
+    mu = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_mu.txt'
+    ))
+
+    w2 = qpc.estimate_weights_multisample(
+        x, mu, relax_moment_condition=.001,
+        solve_kwargs={'solver': cp.OSQP}
+    )
+
+    assert np.allclose(w, w2)
+
+def test_example_norm():
+    w = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_w_norm.txt'
+    ))
+
+    x = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_x.txt'
+    ))
+
+    mu = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_mu.txt'
+    ))
+
+    w2 = qpc.estimate_weights_multisample(
+        x, mu, use_norm=True,
+        solve_kwargs={'solver': cp.ECOS}
+    )
+
+    assert np.allclose(w, w2)
+
+def test_example_alpha3():
+    w = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_w_alpha3.txt'
+    ))
+
+    x = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_x.txt'
+    ))
+
+    mu = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_mu.txt'
+    ))
+
+    w2 = qpc.estimate_weights_multisample(
+        x, mu, alpha=3,
+        solve_kwargs={'solver': cp.ECOS}
+    )
+
+    assert np.allclose(w, w2)
+
+def test_example_kl():
+    w = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_w_kl.txt'
+    ))
+
+    x = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_x.txt'
+    ))
+
+    mu = np.loadtxt(os.path.join(
+        dirname,
+        'test_example_mu.txt'
+    ))
+
+    w2 = qpc.estimate_weights_multisample(
+        x, mu, alpha='kl',
+        solve_kwargs={'solver': cp.ECOS}
+    )
 
     assert np.allclose(w, w2)
